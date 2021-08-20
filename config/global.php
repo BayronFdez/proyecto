b03d5e2d16eefd6eded126c1ca5bcb6e441b5fcd
Ruddy Chambi <kael.ch2@gmail.com>
2021-08-20 12:34:52 -0400

    Proyecto con LAMP

---
+++ config/global.php
@@ -0,0 +1,8 @@
+<?php
+    define("SERVER","localhost");
+    define("USER","rchambi");
+    define("PASSWORD","Admin2021.");
+    define("DATABASE","notas_master");
+    define("SET_NAMES","utf8");
+?>
+
